3c2b27a56b1af08dd2c247c2a34895f84160d475
Zac Garby <zacgarby@icloud.com>
2018-01-16 16:46:37 +0000

    Normalize all tabs

--- static/main.js
+++ static/main.js
@@ -3,39 +3,39 @@ var sock
 // opens a new websocket and sends the code
 // to the server, waiting for response(s)
 function run() {
-    clear()
+	clear()
 
-    sock = new WebSocket(`ws://${window.location.host}/run`)
-    write("waiting for server response...\n")
+	sock = new WebSocket(`ws://${window.location.host}/run`)
+	write("waiting for server response...\n")
 
-    sock.onmessage = handleMessage
+	sock.onmessage = handleMessage
 	sock.onopen = sendCode
 }
 
 // handles a message from the server.
 // write()s it to the output
 function handleMessage(evt) {
-    write(evt.data)
+	write(evt.data)
 }
 
 // sends the value of #input to the server
 function sendCode(evt) {
-    var textarea = document.getElementById("input")
-    var text = textarea.value
+	var textarea = document.getElementById("input")
+	var text = textarea.value
 
-    sock.send(text)
+	sock.send(text)
 }
 
 // writes some text to the output
 function write(text) {
-    var out = document.getElementById("out")
-    out.value += text
+	var out = document.getElementById("out")
+	out.value += text
 }
 
 // clears the output
 function clear() {
-    var out = document.getElementById("out")
-    out.value = ""
+	var out = document.getElementById("out")
+	out.value = ""
 }
 
 // sets up the site -- sets the default values of #input and #out
